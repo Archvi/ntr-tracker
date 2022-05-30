b2470ac10967941326e80d56173b954fbd39f5a6
Archvi <abcalex@gmail.com>
2022-05-30 16:24:25 +0800

    💄 update title

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+        return req.view.render("index", ["title": "Next Trial Run Progress Tracker"])
     }
 
     app.get("hello") { req -> String in
